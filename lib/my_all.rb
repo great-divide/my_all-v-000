0ba5ce9668cce65bcc578480b0a5081db06dd762
Brian Birdwell <bbirdwell@gmail.com>
2018-09-30 23:40:41 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   block_return_value = []
   while i < collection.length
-    collection.all? { |n| n > 3 }
+    collection.all? { |n| n < 3 }
     block_return_value << yield(collection[i])
     i += 1
   end
